e583c6cb0567974df2a55d404704f274f590d23d
Muhammad Saboor Islam <muhammadsaboor119@gmail.com>
2023-10-25 23:38:03 +0500

    hhe

--- src/Components/weather.jsx
+++ src/Components/weather.jsx
@@ -4,10 +4,8 @@ import searchIcon from "./Assets/search.png"
 import clear from "./Assets/clear.png"
 import cloud from "./Assets/cloud.png"
 import drizzle from "./Assets/drizzle.png"
-import Humidity from "./Assets/humidity.png"
 import rain from "./Assets/rain.png"
 import snow from "./Assets/snow.png"
-import Wind from "./Assets/wind.png"
 import Button from './UnitButton'
 import More from "./Assets/more1.png"
 import { ToastContainer, toast } from 'react-toastify';
@@ -17,26 +15,43 @@ import 'react-toastify/dist/ReactToastify.css';
 
 function Weather() {
     let apiKey = "0103960a6b8d6a1f8225dacd3f914659";
-    const [temp,setTemp] = useState("00")
-    const [humid , setHumid] = useState("00")
+    const [temp,setTemp] = useState("0")
+    const [humid , setHumid] = useState("0")
     const [wind , setwind] = useState("0")
     const [location , setlocation] = useState("World")
     const [wicon , setIcon] = useState(clear)
     const [units , setUnits] = useState("C")
-    const [country , setCountry] = useState("PK")
+    const [country , setCountry] = useState("WD")
     const [style , setStyle] = useState(false)
+    const [pressure , setPressure] = useState("0")
+    const [Visibility , setVisibility] = useState("0")
 
+
+
+    const scrollToBottom = () => {
+        window.scrollTo({
+          top: 1100,
+          behavior: 'smooth',
+        });
+    }
     const toggleStyles = () => {
         setStyle(!style);
+        const element = document.getElementById("more");
         if (style === false ){
             document.documentElement.style.height = "1100px"
+            scrollToBottom();
+            element.style.transform = "rotate(180deg)";
         }else{
             document.documentElement.style.height = "800px"
+            element.style.transform = "rotate(0deg)";
         }
       };
-
+      let data = "a";
     const search = async() => {
         const q = document.getElementsByClassName("cityinput");
+        if(q[0].value === location){
+            return 0;
+        }
         if (q[0].value === ""){
             toast("Search Field can't be empty")
             return 0;
@@ -54,6 +69,8 @@ function Weather() {
             setTemp(data.main.temp)
             setlocation(data.name)
             setCountry(data.sys.country)
+            setPressure(data.main.pressure)
+            setVisibility(data.visibility)
             if (data.weather.icon === "01d" || data.weather.icon === "01n"){
                 setIcon(clear)
             }else if (data.weather.icon === "02d" || data.weather.icon === "02n"){
@@ -82,7 +99,7 @@ function Weather() {
             <input type="text" className="cityinput" placeholder='Search'/>
             <ToastContainer />
             <div className="searchIcon" onClick={search}>
-                <img src={searchIcon} alt="search" />
+                <img src="https://img.icons8.com/tapes/40/experimental-search-tapes.png" className='search' alt="search" />
             </div>
         </div>
         <div className="weather-image">
@@ -92,26 +109,48 @@ function Weather() {
         <div className="location">{location} <sup className="superscript-char">{country}</sup></div>
         <div className="dataContainer">
             <div className="element">
-                <img src={Humidity} alt="humid" className='icon'/>
+                <img src="https://img.icons8.com/dotty/80/FFFFFF/moisture.png"  alt="humid" className='icon'/>
                 <div className="data">
                     <div className="humidity-per humidity ">{humid}%</div>
                     <div className="data-text humidity">Humidity</div>
                 </div>
             </div>
             <div className="element">
-                <img src={Wind} alt="wind" className='icon'/>
+                <img src="https://img.icons8.com/external-goofy-solid-kerismaker/96/FFFFFF/external-Wind-weather-goofy-solid-kerismaker.png"   alt="wind" className='icon'/>
                 <div className="data">
                     <div className="wind-rate wind humidity-per humidity">{units=== "C" ? wind : (wind*0.27778).toFixed(2)} {units === "C"? "km/h":"m/s"}</div>
                     <div className="data-text wind">Wind speed </div>
                 </div>
-            </div>
+            </div> 
         </div>
-        
+        <div className={style?"dataContainer2-update":"dataContainer2"}>
+            <div className="element-pressure">
+                <img src="https://img.icons8.com/external-glyph-andi-nur-abdillah/64/FFFFFF/external-Air-Pressure-weather-(glyph)-glyph-andi-nur-abdillah.png"   alt="wind" className='icon'/>
+                <div className="data">
+                    <div className="press-rate wind pressure-per pressure">{units === "C" ?pressure:pressure*100} {units === "C" ? "hPa" : "Pa"}</div>
+                    <div className="data-text pressure">Wind Pressure </div>
+                </div>
+            </div>
+            <div className="element">
+                <img src="https://img.icons8.com/ios-filled/50/FFFFFF/headlight.png"    alt="wind" className='icon'/>
+                <div className="data">
+                    <div className="wind-rate wind humidity-per humidity">{units==="C"?Visibility:(Visibility/1000).toFixed(1)} {units==="C" ? " Meters": " KM"}</div>
+                    <div className="data-text wind">Visibility </div>
+                </div>
+            </div> 
+        </div>
      </div>
-     <img src={More} alt="More" className='more' onClick={toggleStyles}/>
-     <Button setUnit = {setUnits} />
+     <img src={More} alt="More" className='more' id="more" onClick={toggleStyles}/>
+    <Button setUnit = {setUnits} />
      </>
   )
 }
 
 export default Weather
+
+
+
+
+
+
+
